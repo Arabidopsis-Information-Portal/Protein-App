ab51a07e90df724fb1b327f08f9048077366a619
Jason Kuo <jkuo@jcvi.org>
2015-07-31 17:40:50 -0400

    clicking the button now does stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@
 
     /*var showResponse = function showResponse(response) {
       var data = response.obj || response;
-      $(".response", appContext ).html("<pre><code>" + JSON.stringify(data, null, 2 ) + "</code></pre>");
+      $(".response", appContext ).html("<pre><code>" + JSON.stringify(data, null, 2) + "</code></pre>");
     };*/
 
     //shows a table of all proteins
@@ -18,40 +18,46 @@
       // This starts off the html for the table, with headers and classes that will
       // work with Datatables
       var html =
-      "<table class='table display' width='100%'>"+
-          "<thead><tr>"+
-            "<th>Protein Identifier</th><th>More Information</th>"+
-            "</tr></thead>";
+      "<table class='table table-striped' width='100%'>"+
+          "<thead><tr><th>Protein Identifier</th></tr></thead><tbody>";
       // Loops through every protein of the returned json
       for (var i = 0; i < json.obj.result[0].length; i++) {
         // Sets entry as the result
         var entry = json.obj.result[0][i];
         // adds the html for one row in the table
         //First column is just the protein identifier
-        html += "<tr><td>" + entry +
+        html += "<tr><td>" + entry + "</td>" +
           //Second column is a button to show more information about the protein
-          "<a role='button' data-toggle='collapse' href='#" + entry.pathway_id +
-          "'aria-expanded='false' aria-controls='" + entry.pathway_id + "' id='b" + entry.pathway_id + "'>" +
-          "<span class='glyphicon glyphicon-collapse-down' aria-hidden='true'></a></td>" +
-          // The div holds the area that will be expanded, and is given the id of just the pathway ID (e.g. "00010")
-          "<div id='" + entry + "' class='proteinInfo collapse'>Loading...</div></div></td>";
+          "<td><button type='button' class='btn btn-default btn-xs' id='b" + entry + "'>Show more information</button></td></tr>" +
+          // The div holds the area that will be expanded, and its id is the protein identifier
+          "<div id='" + entry + "' class='proteinInfo collapse' data-toggle='collapse'>Loading...</div></td>";
       }
       html += "</tbody></table>";
+      $(".data", appContext).html(html);
 
       // This is called whenever the additional info for a pathway is called to expand
-      $('.proteinInfo', appContext).on('show.bs.collapse', function() {
-
+      $(".btn", appContext).click(function() {
         // Gets the id of the area being expanded (which is the same as the entry which is the protein identifier)
         var id = $(this).attr('id');
+        id = id.substring(1);
+        id = "#"+id;
         // Sets data as the current object so it can be used later when it is no longer the current object
-        //var data = $(this);
-
+        var data = $(id);
         // This function is called to show the data received about a specific protein
         var showProteinInfo = function(json) {
-          html = "<br><ul class-'list-unstyled'>";
-          for (var key in json) {
-            html += "<li><b>" + key + "</b>: " + json.obj.result.key + "</li>\n";
+          var html = "<br><ul class-'list-unstyled'>";
+          var results = json.obj.result[0];
+          for (var proteinCounter = 0; proteinCounter < results.length; proteinCounter++) {
+            var protein = results[proteinCounter];
+            for (var property in protein) {
+              html += "<li><b>" + property + "</b>: " + protein[property] + "</li>\n";
+            }
+            html += "\n";
           }
+          html += "</ul>";
+          data.html(html);
+          console.log(data.html());
+          console.log(html);
         };
 
         // We just finished the function that handles displaying the return info
@@ -66,19 +72,17 @@
           showSearchError
         );
         // Sets the button that show pathway info from expand to collapse
-        $("#b" + id, appContext).html('<span class="glyphicon glyphicon-collapse-up" aria-hidden="true">');
+        $(id, appContext).html('<span class="glyphicon glyphicon-collapse-up" aria-hidden="true">');
       });
 
 
-      $(".data", appContext).empty().html(html);
-
 
       // This is called when the area holding pathway info is called to collapse
-      $('.pinfo', appContext).on('hide.bs.collapse', function() {
+      $('.proteinInfo', appContext).on('hide.bs.collapse', function() {
         // Gets the id of the area
         var id = $(this).attr('id');
         // Sets the button that show pathway info from collapse to expand
-        $('#b'+id, appContext).html('<span class="glyphicon glyphicon-collapse-down" aria-hidden="true">');
+        $(id, appContext).html('<span class="glyphicon glyphicon-collapse-down" aria-hidden="true">');
       });
     };
 
@@ -91,12 +95,8 @@
       var html = "<div class='alert alert-danger' role='alert'>" + json.obj.message + "</div>";
       $("#error", appContext).html(html);
     };
-    /*
-    var params = {
-      Identifier: "AT4G09000.1",
-      Output: "all"
-    };
-    */
+
+
     Agave.api.adama.list(
       {namespace: "jk-dev", service: "protein_api_v0.1"},
       showProteinList,
